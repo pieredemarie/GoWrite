3cf4dde3c3dc63034ec58f4263298e9681dbccaa
Khachatryan Artem <65036324+pieredemarie@users.noreply.github.com>
2026-09-06 09:47:50 +0300

    added ArticleService interface, functions for articleService struct

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,1 +1,13 @@
 package handler
+
+import (
+	"context"
+	"gowrite/internal/model"
+)
+
+type ArticleService interface {
+	CreateArticle(ctx context.Context, title, contentMD string) (slug, token string, error error)
+	GetArticleBySlug(ctx context.Context, slug string) (model.Article, error)
+	UpdateArticle(ctx context.Context, slug, token, title, contentMD string) error
+	DeleteArticle(ctx context.Context, slug, token string) error
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -11,3 +11,29 @@ type ArticleRepository interface {
 	Delete(ctx context.Context, slug string) error
 	Update(ctx context.Context, article model.Article) error
 }
+
+type articleService struct {
+	repo ArticleRepository
+}
+
+func NewArticleService(repo ArticleRepository) *articleService {
+	return &articleService{
+		repo: repo,
+	}
+}
+
+func (s *articleService) CreateArticle(ctx context.Context, title, contentMD string) (slug, token string, error error) {
+
+}
+
+func (s *articleService) GetArticleBySlug(ctx context.Context, slug string) (model.Article, error) {
+
+}
+
+func (s *articleService) UpdateArticle(ctx context.Context, slug, token, title, contentMD string) error {
+
+}
+
+func (s *articleService) DeleteArticle(ctx context.Context, slug, token string) error {
+
+}
